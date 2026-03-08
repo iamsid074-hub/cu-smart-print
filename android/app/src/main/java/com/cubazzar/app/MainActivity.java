e907fef9395bf3401391a289870592eac7d16721
Anshu <iamsid074@gmail.com>
2026-03-09 03:55:15 +0530

    build: added capacitor ios and android native platform wrappers

---
+++ android/app/src/main/java/com/cubazzar/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.cubazzar.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cubazzar.app',
+  appName: 'CU Bazzar',
+  webDir: 'dist'
+};
+
+export default config;
