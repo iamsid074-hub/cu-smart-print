342357222c45edaf5f9df652bdf8d507c3556ea8
Anshu <iamsid074@gmail.com>
2026-03-25 20:47:34 +0530

    up

--- src/components/TopDynamicIsland.tsx
+++ src/components/TopDynamicIsland.tsx
@@ -498,8 +498,9 @@ export default function TopDynamicIsland({ onSell }: TopDynamicIslandProps) {
                 animate={{ opacity: 1, y: 0, scale: 1 }}
                 exit={{ opacity: 0, y: -8, scale: 0.95 }}
                 transition={{ type: "spring", stiffness: 400, damping: 30 }}
-                className="pointer-events-auto absolute top-full mt-2 left-1/2 -translate-x-1/2"
+                className="pointer-events-auto absolute left-1/2 -translate-x-1/2"
                 style={{
+                  top: height + 8,
                   width: "min(360px, calc(100vw - 32px))",
                   background: "rgba(0,0,0,0.92)",
                   backdropFilter: "blur(40px) saturate(180%)",
@@ -650,6 +651,7 @@ export default function TopDynamicIsland({ onSell }: TopDynamicIslandProps) {
               </motion.div>
             )}
           </AnimatePresence>
+          </div>{/* close relative wrapper */}
         </div>
       </div>
     </>
