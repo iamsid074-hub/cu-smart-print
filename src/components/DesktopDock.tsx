8a2238fd087b3c83b39455af22101f2bb767f9d1
Anshu <iamsid074@gmail.com>
2026-05-05 19:03:50 +0530

    UI: Updated Settings icon to the new black and white version

--- src/components/DesktopDock.tsx
+++ src/components/DesktopDock.tsx
@@ -13,7 +13,7 @@ const dockItemsRaw: DockItem[] = [
   { name: "Games",    path: "/games",            img: "/dock-games.png" },
   { name: "Cart",     path: "/cart",             img: "/dock-cart.png" },
   { name: "Profile",  path: "/profile",          img: "/dock-profile.png", imgClass: "scale-[1.15]" },
-  { name: "Settings", path: "/settings",         img: "/dock-settings.png", imgClass: "-translate-x-[5%] -translate-y-[5%]" },
+  { name: "Settings", path: "/settings",         img: "/dock-settings.png" },
 ];
 
 export const dockItems = dockItemsRaw;
