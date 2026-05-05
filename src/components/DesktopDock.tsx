9c003fcf97788f66fa8785d34732a8b86cadd22e
Anshu <iamsid074@gmail.com>
2026-05-05 18:46:52 +0530

    UI: Reverted Settings icon scaling to match Home icon border

--- src/components/DesktopDock.tsx
+++ src/components/DesktopDock.tsx
@@ -13,7 +13,7 @@ const dockItemsRaw: DockItem[] = [
   { name: "Games",    path: "/games",            img: "/dock-games.png" },
   { name: "Cart",     path: "/cart",             img: "/dock-cart.png" },
   { name: "Profile",  path: "/profile",          img: "/dock-profile.png", imgClass: "scale-[1.15]" },
-  { name: "Settings", path: "/settings",         img: "/dock-settings.png", imgClass: "scale-[1.25] -translate-x-[4%] -translate-y-[4%]" },
+  { name: "Settings", path: "/settings",         img: "/dock-settings.png" },
 ];
 
 export const dockItems = dockItemsRaw;
