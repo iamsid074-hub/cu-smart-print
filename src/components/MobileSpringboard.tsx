09b22be889a498483d22a1d6e164e44745d372fe
Anshu <iamsid074@gmail.com>
2026-05-18 03:43:21 +0530

    fix: restore missing AnimatePresence tag in springboard render

--- src/components/MobileSpringboard.tsx
+++ src/components/MobileSpringboard.tsx
@@ -459,6 +459,7 @@ export default function MobileSpringboard() {
             </motion.div>
           </motion.div>
         )}
+      </AnimatePresence>
       {showOnboarding && (
         <OnboardingTour onClose={() => setShowOnboarding(false)} />
       )}
